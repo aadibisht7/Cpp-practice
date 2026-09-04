9e83d5db4be2d276c3bb606c193c014662732c48
Aadibisht <adityabisht.bg@gmail.com>
2026-09-04 18:59:16 +0530

    Pivot Elements

---
+++ Pivot Element/pivotelement.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int pivotElement(int arr[], int size){
+    int s = 0;
+    int e = size - 1;
+    int mid = s + (e-s)/2;
+
+    while(s < e){
+        if(arr[mid] >= arr[0]){
+            s = mid + 1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return s;
+}
+
+int main(){
+    int arr[5] = {3, 8, 10, 1, 2};
+    int size = 5;
+
+    cout << "Pivot is: " << pivotElement(arr, size);
+}
